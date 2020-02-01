9ab7355c52ff7467391fd828345247e58a6deead
Gauthier Billot <gogoprog@gmail.com>
2020-02-01 15:23:39 +0100

    Wip

--- src/Main.hx
+++ src/Main.hx
@@ -3,18 +3,21 @@ import js.Browser.window;
 
 class Main {
     static function main() {
-        var doc = window.document;
-        var c:js.html.CanvasElement = cast doc.createElement("canvas");
+        var w = window;
+        var c:js.html.CanvasElement = cast w.document.querySelector("canvas");
         var time:Float = 0;
         var screenSize = 512;
-        doc.body.appendChild(c);
         c.width = c.height = screenSize;
         var ctx:js.html.CanvasRenderingContext2D = c.getContext("2d");
+        var pt:Float = 0;
+        var a:Float = 0;
         function loop(t:Float) {
-            time = t/1000;
+            var dt = (t-pt) / 1000;
+            pt = t;
+            a += 32 * dt;
             ctx.clearRect(0, 0, screenSize, screenSize);
-            ctx.fillRect(time * 50, 0, 32, 32);
-            window.requestAnimationFrame(loop);
+            ctx.fillRect(a, 0, 32, 32);
+            w.requestAnimationFrame(loop);
         }
         loop(0);
     }
