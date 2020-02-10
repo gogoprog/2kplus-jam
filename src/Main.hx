6993049ed8ebc0a3e9bb16a83023913fc2d6838c
Gauthier Billot <gauthier.billot@fishingcactus.com>
2020-02-10 22:00:54 +0100

    Menus

--- src/Main.hx
+++ src/Main.hx
@@ -23,7 +23,7 @@ class Main {
         var lastFireTime:Float = 0;
         var rseed;
         var mx;
-        var life = 100;
+        var life = 10;
         var mustFire:Bool;
         var bullets:Array<Bullet> = [];
         var enemies:Array<Enemy> = [];
@@ -32,11 +32,16 @@ class Main {
         var m = Math;
         var abs = m.abs;
         var sin = m.sin;
+        var state = 0;
+        ctx.font = "20px monospace";
         function col(n) {
             ctx.fillStyle = n;
         }
         function alpha(n) {
             ctx.globalAlpha = n;
+        }
+        function scale(s) {
+            ctx.scale(s, s);
         }
         function drawRect(x:Float, y:Float, w, h) {
             ctx.fillRect(x-w/2, y-h/2, w, h);
@@ -76,7 +81,7 @@ class Main {
             var n = bullets.length;
 
             for(i in 0...n) {
-                if(abs(bullets[i].y) > 1024) {
+                if(abs(bullets[i].y) > screenSize*2) {
                     n = i;
                     break;
                 }
@@ -91,80 +96,100 @@ class Main {
             col("white");
 
             for(i in 0...50) {
-                drawRect(random() * 512, (random() * 512 + t * (random() * 0.2)) % 512, 2, 2);
+                drawRect(random() * screenSize, (random() * screenSize + t * (random() * 0.2)) % screenSize, 2, 2);
             }
 
-            alpha(1);
+            if(state == 0) {
+                scale(4);
+                ctx.fillText("SHIP2k", 24, 32);
+                scale(1/2);
+                ctx.fillText("Click to play!", 42, 232);
+                scale(1/2);
+                ctx.fillText("Best score: ", 32, 232);
 
-            for(b in bullets) {
-                b.y += 5 * b.d;
-                col(b.d == -1 ? "lightgreen" : "red");
-                drawRect(b.x, b.y, 4, 12);
-
-                if(b.d > 0) {
-                    if(abs(b.y - 420) + abs(b.x-mx) < 32) {
-                        life--;
-                        b.y = 999;
-                        alpha(0.5);
-                    }
+                if(mustFire) {
+                    state++;
                 }
-            }
-
-            drawShip(mx, 460);
-
-            if(mustFire) {
-                if(time - lastFireTime > 10) {
-                    fire(mx, 420, -1);
-                    lastFireTime = time;
-                }
-            }
-
-            for(e in enemies) {
-                var x = e.x + sin(++e.t / 100) * 100;
-                var y =  -64 + e.t;
-
-                if(e.t % 30 == 0) {
-                    fire(x, y, 1);
-                }
-
+            } else if(state == 1) {
                 alpha(1);
 
                 for(b in bullets) {
-                    if(b.d < 0) {
-                        if(abs(b.y - y) + abs(b.x-x) < 32) {
-                            b.y = -999;
-                            e.life -= 1;
-                            alpha(0.5);
+                    b.y += 10 * b.d;
+                    col(b.d == -1 ? "lightgreen" : "red");
+                    drawRect(b.x, b.y, 4, 12);
 
-                            if(e.life < 1) {
-                                e.t = 666;
-                            }
+                    if(b.d > 0) {
+                        if(abs(b.y - 420) + abs(b.x-mx) < 32) {
+                            life--;
+                            b.y = 999;
+                            alpha(0.5);
                         }
                     }
                 }
 
-                drawEnemy(x, y);
-            }
+                drawShip(mx, 460);
 
-            rseed = time;
-
-            if((time % 100) == 0) {
-                var n = enemies.length;
-
-                for(e in 0...n) {
-                    if(enemies[e].t > 666) {
-                        n = e;
-                        break;
+                if(mustFire) {
+                    if(time - lastFireTime > 10) {
+                        fire(mx, 420, -1);
+                        lastFireTime = time;
                     }
                 }
 
-                enemies[n] = {x: 512 * random(), t:0, life:5};
+                for(e in enemies) {
+                    var x = e.x + sin(++e.t / 100) * 100;
+                    var y = -64 + e.t;
+
+                    if(e.t % 60 == 0) {
+                        fire(x, y, 1);
+                    }
+
+                    alpha(1);
+
+                    for(b in bullets) {
+                        if(b.d < 0) {
+                            if(abs(b.y - y) + abs(b.x-x) < 32) {
+                                b.y = -999;
+                                e.life -= 1;
+                                alpha(0.5);
+
+                                if(e.life < 1) {
+                                    e.t = 666;
+                                }
+                            }
+                        }
+                    }
+
+                    drawEnemy(x, y);
+                }
+
+                rseed = time;
+
+                if((time % 150) == 0) {
+                    var n = enemies.length;
+
+                    for(e in 0...n) {
+                        if(enemies[e].t > screenSize) {
+                            n = e;
+                            break;
+                        }
+                    }
+
+                    enemies[n] = {x: screenSize * random(), t:0, life:5};
+                }
+
+                alpha(1);
+                col("#455");
+                drawRect(256, 500, screenSize, 24);
+                col("white");
+                var str = "";
+
+                for(i in 0...10) { str+= i< life ? "O":"_"; }
+
+                ctx.fillText("HP: [" + str + "]", 12, 506);
+                time++;
             }
 
-            col("white");
-            ctx.font = "20px Courier New";
-            ctx.fillText("HP: " + life, 12, 500);
-            time++;
             w.requestAnimationFrame(loop);
         }
         loop(0);
